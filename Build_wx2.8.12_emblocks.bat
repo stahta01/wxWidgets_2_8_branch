671a8ac2b42c5e6d9617cfc4a33cb6d2cb016d3d
Tim S <stahta01@users.sourceforge.net>
2016-06-15 09:35:27 -0400

    * build: Added "CFG=_wxeb" to batch file.

--- Build_wx2.8.12_emblocks.bat
+++ Build_wx2.8.12_emblocks.bat
@@ -9,15 +9,15 @@ SET _CPPFLAGS="-D_WIN32_IE=0x0601"
 SET _CXXFLAGS="-fno-strict-aliasing -fpermissive -fno-keep-inline-dllexport -Wno-unused-local-typedefs"
 
 REM clean
-REM mingw32-make -f makefile.gcc CPPFLAGS=%_CPPFLAGS% CXXFLAGS=%_CXXFLAGS% USE_XRC=1 SHARED=1 MONOLITHIC=1 BUILD=release UNICODE=1 VENDOR=eb clean
+REM mingw32-make -f makefile.gcc CFG=_wxeb CPPFLAGS=%_CPPFLAGS% CXXFLAGS=%_CXXFLAGS% USE_XRC=1 SHARED=1 MONOLITHIC=1 BUILD=release UNICODE=1 VENDOR=eb clean
 REM
 REM setup_h
-    mingw32-make -f makefile.gcc CPPFLAGS=%_CPPFLAGS% CXXFLAGS=%_CXXFLAGS% USE_XRC=1 SHARED=1 MONOLITHIC=1 BUILD=release UNICODE=1 VENDOR=eb setup_h
+    mingw32-make -f makefile.gcc CFG=_wxeb CPPFLAGS=%_CPPFLAGS% CXXFLAGS=%_CXXFLAGS% USE_XRC=1 SHARED=1 MONOLITHIC=1 BUILD=release UNICODE=1 VENDOR=eb setup_h
     PAUSE
 REM build_cfg_file
-    mingw32-make -f makefile.gcc CPPFLAGS=%_CPPFLAGS% CXXFLAGS=%_CXXFLAGS% USE_XRC=1 SHARED=1 MONOLITHIC=1 BUILD=release UNICODE=1 VENDOR=eb build_cfg_file
+    mingw32-make -f makefile.gcc CFG=_wxeb CPPFLAGS=%_CPPFLAGS% CXXFLAGS=%_CXXFLAGS% USE_XRC=1 SHARED=1 MONOLITHIC=1 BUILD=release UNICODE=1 VENDOR=eb build_cfg_file
 REM PAUSE
-    mingw32-make -f makefile.gcc CPPFLAGS=%_CPPFLAGS% CXXFLAGS=%_CXXFLAGS% USE_XRC=1 SHARED=1 MONOLITHIC=1 BUILD=release UNICODE=1 VENDOR=eb
+    mingw32-make -f makefile.gcc CFG=_wxeb CPPFLAGS=%_CPPFLAGS% CXXFLAGS=%_CXXFLAGS% USE_XRC=1 SHARED=1 MONOLITHIC=1 BUILD=release UNICODE=1 VENDOR=eb
 REM BUILD=debug
 REM >log.txt 2>&1
 REM 
